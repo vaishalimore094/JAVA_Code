74bfd2deee97f4f13bc9f0973505838e258a6b85
vaishalimore094 <vaishalimore094@gmail.com>
2025-04-10 14:29:13 +0530

    Add files via upload

---
+++ LogicalProgram_Number/SwapTwo_Number/Swap_withthird_Number.java
@@ -0,0 +1,16 @@
+package Swap_two_number;
+
+public class Swap_withthird_Number {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("print a value  "+a);
+		System.out.println("print b value "+b);
+	}
+
+}
