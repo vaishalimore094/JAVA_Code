f663193893fa62cdb29114257653f5f40e736f00
vaishalimore094 <vaishalimore094@gmail.com>
2025-04-11 12:42:30 +0530

    Add files via upload

---
+++ LogicalProgram_Number/OddEven_Number/Odd_evenNumber.java
@@ -0,0 +1,23 @@
+package odd_evenNumber;
+
+public class Odd_evenNumber {
+
+	public static void main(String[] args) {
+		int num=22;
+		
+		System.out.println(num%2);// will give Remainder
+	  // System.out.println(num/2);// will give Quotation
+
+		
+	if(num%2==0)
+	{
+		System.out.println("Number is Even");
+	}
+	else
+	{
+	System.out.println("Number is odd");	
+	}
+  }
+	}
+
+
