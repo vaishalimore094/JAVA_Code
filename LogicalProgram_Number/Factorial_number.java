3c5615265bf55ade0ffc4a2ae25d014accf59b28
vaishalimore094 <vaishalimore094@gmail.com>
2025-06-17 16:04:14 +0530

    Add files via upload
    Factorial_Number

---
+++ LogicalProgram_Number/Factorial_number.java
@@ -0,0 +1,31 @@
+package Factorial_Number;
+
+public class Factorial_number {
+
+	public static void main(String[] args) {
+		{
+			
+//			3!
+//			3*2*1=
+			
+			int num=3;
+			int fact=1;//3
+			
+			for (int i=num;i>=1;i--)//
+			{
+				fact=fact*i; //
+				
+			}
+			
+			System.out.println("factorial of number is "+fact);
+			
+			
+
+		}
+
+	
+
+
+	}
+
+}
